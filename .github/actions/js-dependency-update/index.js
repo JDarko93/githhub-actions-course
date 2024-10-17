50b1897e7e1c7ec856862a3d57e650c3c6883435
jesse <cyberdarko24@gmail.com>
2024-10-17 12:41:32 +0100

    added logic to create PR

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,18 +1,23 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 const validateBranchName = ({branchName}) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({dirName}) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-  const baseBranch = core.getInput('base-branch');
-  const targetBranch = core.getInput('target-branch');
-  const ghToken = core.getInput('gh-token');
-  const workingDir = core.getInput('working-directory');
+  const baseBranch = core.getInput('base-branch', {required: true});
+  const targetBranch = core.getInput('target-branch', {required: true});
+  const ghToken = core.getInput('gh-token', {required: true});
+  const workingDir = core.getInput('working-directory', {required: true});
   const debug = core.getBooleanInput('debug');
 
-  core.setSecret(ghToken);
+  const commonExecOpts = {
+    cwd: workingDir
+  }
 
+  core.setSecret(ghToken);
+ 
   if (!validateBranchName({branchName: baseBranch})) {
     core.setFailed('Invalid branch name. Branch names should only include a-z 0-9 special characters')
     return;
@@ -33,16 +38,47 @@ async function run() {
   core.info(`[js-dependency-update] : working directory is ${workingDir}`);
   
   await exec.exec ('npm update', [], {
-    cwd: workingDir
+    ...commonExecOpts
   });
 
   const gitStatus = await exec.getExecOutput('git status -s package*json', [], {
-    cwd: workingDir
+    ...commonExecOpts
   });
-  cwd: workingDir
 
   if (gitStatus.stdout.length > 0) {
-    core.info('[js-dependency-update] : There are updates available!')
+    core.info('[js-dependency-update] : There are updates available!');
+    await exec.exec('git config --global user.name "gh-automation"');
+    await exec.exec('git config --global user.mail "gh-automation@email.com"');
+    await exec.exec('git checkout -b ${targetBranch}', [], {
+      ...commonExecOpts,
+    });
+    await exec.exec('git add package.json package-lock.json', [], {
+      ...commonExecOpts,
+    });
+    await exec.exec('git commit -m "chore: update dependencies', [], {
+      ...commonExecOpts,
+    });
+    await exec.exec('git push -u origin ${targetBranch} --force', [], {
+      ...commonExecOpts,
+    });
+
+    const octokit = github.getOctokit(ghToken);
+
+    try {
+    await octokit.rest.pulls.create({
+      owner: 'github.context.repo.owner',
+      repo: 'github.context.repo.repo',
+      title: 'Update NPM dependencies',
+      body: 'This pull request updates NPM packages',
+      base: baseBranch,
+      head: targetBranch
+      });
+    } catch (e) {
+      core.error('[js-dependency-update] : Something went wrong while creating PR. Check logs below.')
+      core.setFailed(e.message);
+      core.error(e);
+     
+    }
   } else {
     core.info('[js-dependency-update] : No updates at this point in time.')
   }
